c5e853389467bd770540108a075f5ac97dffd577
hsimov <hsimov@gmail.com>
2023-02-08 16:38:01 +0200

    first commit

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,7 +7,16 @@ def store(request):
     return render(request, 'store/Store.html', context)
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False) # To study
+        items = order.orderitem_set.all() # To study
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0} # for unauthenticated user
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/Cart.html', context)
 
 def checkout(request):

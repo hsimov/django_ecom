49bbd86511b89334fcbd78de2e12d24675730e55
hsimov <hsimov@gmail.com>
2023-02-24 15:17:40 +0200

    Guest User Cart added

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -4,20 +4,12 @@ import json
 import datetime
 
 from .models import *
-from .utils import cookieCart
+from .utils import cookieCart, cartData, guestOrder
 
 
 def store(request):
-    if request.user.is_authenticated:
-        customer=request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False) # To study
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        cookieData = cookieCart(request)
-        cartItems = cookieData['cartItems']
-        order = cookieData['order']
-        items = cookieData['items']
+    data = cartData(request)
+    cartItems = data['cartItems']
 
     products = Product.objects.all()
     context = {'products':products, 'cartItems': cartItems}
@@ -25,35 +17,20 @@ def store(request):
 
 def cart(request):
 
-    if request.user.is_authenticated:
-        customer=request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False) # To study
-        items = order.orderitem_set.all() # To study
-        cartItems = order.get_cart_items
-    else:
-        cookieData = cookieCart(request)
-        cartItems = cookieData['cartItems']
-        order = cookieData['order']
-        items = cookieData['items']
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
     
-                
-
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'store/Cart.html', context)
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer=request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False) # To study
-        items = order.orderitem_set.all() # To study
-        cartItems = order.get_cart_items
-    else:
-        cookieData = cookieCart(request)
-        cartItems = cookieData['cartItems']
-        order = cookieData['order']
-        items = cookieData['items']
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
         
-
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'store/Checkout.html', context)
 
@@ -84,36 +61,38 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False) # what is 'safe'?
 
 def processOrder(request):
-	transaction_id = datetime.datetime.now().timestamp()
-	data = json.loads(request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-	else:
-		print("Not authenticated")
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        
+    else:
+        customer, order = guestOrder(request, data)
 
-	total = float(data['form']['total'])
-	order.transaction_id = transaction_id
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
 
-	if total == order.get_cart_total: # Check if the value was maniuplated
-		order.complete = True
-	order.save()
+    if total == order.get_cart_total: # Check if the value was maniuplated
+        order.complete = True
+    order.save()
 
-	if order.shipping == True:
-		ShippingAddress.objects.create(
-		customer=customer,
-		order=order,
-		address=data['shipping']['address'],
-		city=data['shipping']['city'],
-		state=data['shipping']['state'],
-		zipcode=data['shipping']['zipcode'],
-		)
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+		    order=order,
+		    address=data['shipping']['address'],
+		    city=data['shipping']['city'],
+		    state=data['shipping']['state'],
+		    zipcode=data['shipping']['zipcode'],
+        )
 
-	return JsonResponse('Payment submitted..', safe=False)
+    return JsonResponse('Payment complete!', safe=False)
 
 
 
+
+        
+        
     
-
-
